5ca6054bac4372dac2d480e9ed0622667c0bb87b
cgoedel <christoph.goedel-pavlik@helloly.com>
2025-07-01 11:20:06 +0200

    Add CDATA encoding for authinfo in Nic.at transfer request

--- Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppTransferRequest.php
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppTransferRequest.php
@@ -1,42 +1,57 @@
 <?php
 namespace Metaregistrar\EPP;
 
+use DOMException;
 
 class atEppTransferRequest extends eppTransferRequest
 {
     use atEppCommandTrait;
 
+    /**
+     * @var atEppExtensionChain|null
+     */
     protected $atEppExtensionChain = null;
 
-    function __construct($operation, $object,?atEppExtensionChain $atEppExtensionChain=null)
+    public function __construct($operation, $object, ?atEppExtensionChain $atEppExtensionChain = null, bool $usecdata = false)
     {
         $this->atEppExtensionChain = $atEppExtensionChain;
-        parent::__construct($operation, $object);
+        parent::__construct($operation, $object, $usecdata);
         $this->setAtExtensions();
         $this->addSessionId();
     }
 
+    /**
+     * @throws DOMException
+     */
+    public function setDomainRequest(eppDomain $domain)
+    {
+        #
+        # Object create structure
+        #
+        $transfer = $this->createElement('transfer');
+        $transfer->setAttribute('op', self::OPERATION_REQUEST);
 
-	public function setDomainRequest(eppDomain $domain) {
-		#
-		# Object create structure
-		#
-		$transfer = $this->createElement('transfer');
-		$transfer->setAttribute('op', self::OPERATION_REQUEST);
-		$this->domainobject = $this->createElement('domain:transfer');
-		$this->domainobject->appendChild($this->createElement('domain:name', $domain->getDomainname()));
-		if ($domain->getPeriod()) {
-			$domainperiod = $this->createElement('domain:period', $domain->getPeriod());
-			$domainperiod->setAttribute('unit', eppDomain::DOMAIN_PERIOD_UNIT_Y);
-			$this->domainobject->appendChild($domainperiod);
-		}
-		if (strlen($domain->getAuthorisationCode())) {
-			$authinfo = $this->createElement('domain:authInfo');
-			$authinfo->appendChild($this->createElement('domain:pw', $domain->getAuthorisationCode()));
-			$this->domainobject->appendChild($authinfo);
-		}
-		$transfer->appendChild($this->domainobject);
-		$this->getCommand()->appendChild($transfer);
-	}
+        $domainobject = $this->createElement('domain:transfer');
+        $domainobject->appendChild($this->createElement('domain:name', $domain->getDomainname()));
+        if ($domain->getPeriod()) {
+            $domainperiod = $this->createElement('domain:period', $domain->getPeriod());
+            $domainperiod->setAttribute('unit', eppDomain::DOMAIN_PERIOD_UNIT_Y);
+            $domainobject->appendChild($domainperiod);
+        }
+        if (strlen($domain->getAuthorisationCode())) {
+            $authinfo = $this->createElement('domain:authInfo');
+
+            if ($this->useCdata()) {
+                $pw = $authinfo->appendChild($this->createElement('domain:pw'));
+                $pw->appendChild($this->createCDATASection($domain->getAuthorisationCode()));
+            } else {
+                $authinfo->appendChild($this->createElement('domain:pw', $domain->getAuthorisationCode()));
+            }
+
+            $domainobject->appendChild($authinfo);
+        }
+        $transfer->appendChild($domainobject);
+        $this->getCommand()->appendChild($transfer);
+    }
 
 }
